ffe1561d329b1939262e466ba1004b3fedb323db
lamquangtruongnguyen <tnguyen10@my.hpu.edu>
2023-10-30 12:23:20 -1000

    add header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,12 +4,12 @@ import "./Header.css";
 const Header = () => {
   return (
     <header>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-info-subtle">
         <Container>
-          <Navbar.Brand href="#home">Zero Waste Campaign</Navbar.Brand>
+          <Navbar.Brand href="#home" className="text-uppercase fw-bold text-success fs-3">Zero Waste Campaign</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="me-auto fw-semibold fs-5">
               <Nav.Link href="#home">Sign In</Nav.Link>
               <Nav.Link href="#link">Sign Up</Nav.Link>
             </Nav>
