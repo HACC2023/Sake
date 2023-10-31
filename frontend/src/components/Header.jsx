a4bad7623f3baa099a02f75f9707ea8722b968ca
lamquangtruongnguyen <tnguyen10@my.hpu.edu>
2023-10-30 14:03:08 -1000

    add header, hero, routers

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,26 @@
 import { Container, Navbar, Nav } from "react-bootstrap";
-import "./Header.css";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
 
 const Header = () => {
   return (
     <header>
-      <Navbar expand="lg" className="bg-info-subtle">
+      <Navbar expand="lg" bg="light" sticky="top">
         <Container>
-          <Navbar.Brand href="#home" className="text-uppercase fw-bold text-success fs-3">Zero Waste Campaign</Navbar.Brand>
+          <Navbar.Brand
+            href="/"
+            className="text-uppercase fw-bold text-success fs-3"
+          >
+            Zero Waste Campaign
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto fw-semibold fs-5">
-              <Nav.Link href="#home">Sign In</Nav.Link>
-              <Nav.Link href="#link">Sign Up</Nav.Link>
+            <Nav className="ms-auto fw-semibold fs-5">
+              <Nav.Link href="/signin">
+                <FaSignInAlt /> Sign In
+              </Nav.Link>
+              <Nav.Link href="/signup">
+                <FaSignOutAlt /> Sign Up
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import Home from "./screens/Home.jsx";
+import SignIn from "./screens/SignIn.jsx";
+import SignUp from "./screens/SignUp.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
