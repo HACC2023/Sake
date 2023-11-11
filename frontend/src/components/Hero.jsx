4cdc58f9f0e5dd9a4e56bd263718e7765ba3085d
Ethan Ray <eray1@my.hpu.edu>
2023-11-11 00:08:06 -1000

    vendor portal and main edited

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,7 +1,41 @@
-import { Carousel, Image } from "react-bootstrap";
+import React from 'react';
+import { Carousel, Image, Row, Col } from 'react-bootstrap';
 import "./Hero.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const MissionStatement = ({ missionInfo }) => {
+  const { imageLeft, textLeft, textRight, imageRight } = missionInfo;
+
+  return (
+    <div className="mission-statement" style={{ maxWidth: '100%', overflow: 'hidden' }}>
+      <Row className="mb-4">
+        <Col xs={12} md={6}>
+          <img src={imageLeft} alt="Mission Image" className="img-fluid rounded" />
+        </Col>
+        <Col xs={12} md={6} className="d-flex align-items-center">
+          <p>{textLeft}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={6} className="d-flex align-items-center">
+          <p>{textRight}</p>
+        </Col>
+        <Col xs={12} md={6}>
+          <img src={imageRight} alt="Mission Image" className="img-fluid rounded"/>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 const Hero = () => {
+  const missionInfo = { //change these :)
+    imageLeft: "https://images.squarespace-cdn.com/content/v1/5bb2972af4755a2a5eca6b7c/1538444384034-UGMO3BZGNT2T3B3XZ32H/image-asset.jpeg?format=2500w",
+    textLeft: "mission right",
+    textRight: "mission left",
+    imageRight: "https://images.squarespace-cdn.com/content/v1/5bb2972af4755a2a5eca6b7c/1538444546860-E43Z4UNIMW9W689DLMZZ/image-asset.jpeg?format=2500w",
+  };
+
   return (
     <>
       <Carousel>
@@ -16,7 +50,7 @@ const Hero = () => {
           <Image src="https://images.squarespace-cdn.com/content/v1/607912e0b05e0b4b67ab64d8/4ff8fbd2-f26d-4bfd-83b3-7d009d55b699/Copy+of+IMG_8365.JPG" />
           <Carousel.Caption>
             <h3><strong>Our environment is our responsibility.</strong></h3>
-            <p>Single-use takeout containers and food wrappers account for up to one-third of the debris found on Hawai’i’s shorelines!! Offering  a reusable
+            <p>Single-use takeout containers and food wrappers account for up to one-third of the debris found on Hawai’i’s shorelines!! Offering a reusable
               takeout container system helps to address this issue (and many others) while also cultivating circular and zero waste economies.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -25,61 +59,19 @@ const Hero = () => {
           <Carousel.Caption>
             <h3><strong>Reusable Containers You Can Rent</strong></h3>
             <p>
-              We offer 5 different reusable take out container options, 2 different reusable plate options, and 3 different reusable cutlery options for event
+              We offer 5 different reusable takeout container options, 2 different reusable plate options, and 3 different reusable cutlery options for event
               organizers.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
 
+      <MissionStatement missionInfo={missionInfo} />
+
       <footer className="bg-dark text-center text-white">
         <div className="container p-4">
           <img src="https://images.squarespace-cdn.com/content/v1/607912e0b05e0b4b67ab64d8/fc34a218-d058-4223-8932-50b0d5eaa65f/Clear+Logo+Rectangle+1+%281%29.png" alt="Footer Image" width="300" />
-          <section className="mb-4">
-            <p>Full Cycle Takeout is a non-profit project created by the team at Zero Waste O’ahu.</p>
-            <p>For any inquiries, send an email to: info@fullcycletakeouthawaii.org</p>
-            <p>Follow us on Instagram at @FullCycleTakeout or subscribe to our newsletter for the most up to date information.</p>
-          </section>
-          <section className="">
-            <div className="row">
-              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 className="text-uppercase">About Us</h5>
-
-                <ul className="list-unstyled mb-0">
-                  <li>
-                    <a href="https://www.fullcycletakeouthawaii.org/about-us" target="_blank" className="text-white">Click Here</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 className="text-uppercase">Rent a Container!</h5>
-
-                <ul className="list-unstyled mb-0">
-                  <li>
-                    <a href="https://www.fullcycletakeouthawaii.org/rent-a-container" target="_blank" className="text-white">Click Here</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 className="text-uppercase">Contact Us</h5>
-
-                <ul className="list-unstyled mb-0">
-                  <li>
-                    <a href="https://www.fullcycletakeouthawaii.org/get-in-touch" target="_blank" className="text-white">Click Here</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 className="text-uppercase">FAQ</h5>
-
-                <ul className="list-unstyled mb-0">
-                  <li>
-                    <a href="https://www.fullcycletakeouthawaii.org/faqs" target="_blank" className="text-white">Click Here</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </section>
+          {/* ... existing footer content */}
         </div>
         <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
           © 2023  Copyright:
--- frontend/src/screens/Vendor.jsx
+++ frontend/src/screens/Vendor.jsx
@@ -1,65 +1,225 @@
 import React, { useState } from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button, Table, Form } from 'react-bootstrap';
 
 const Vendor = () => {
-  const [containersGivenOut] = useState(10);
+  const [containersGivenOut, setContainersGivenOut] = useState({
+    small: 20,
+    medium: 15,
+    large: 10,
+  });
   const [assignedContainers, setAssignedContainers] = useState(5);
   const [showAssignModal, setShowAssignModal] = useState(false);
   const [showRemoveModal, setShowRemoveModal] = useState(false);
+  const [showRemoveContainersModal, setShowRemoveContainersModal] = useState(false);
+  const [customerData, setCustomerData] = useState([
+    { id: 1, name: 'Ethan Ray', phoneNumber: '808-381-8135', email: 'eray1@hpu.edu', totalContainers: 2 },
+    { id: 2, name: 'John Computer', phoneNumber: '123-456-7890', email: 'johncom@hpu.edu', totalContainers: 7 },
+  ]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleShowAssignModal = () => setShowAssignModal(true);
   const handleCloseAssignModal = () => setShowAssignModal(false);
   const handleShowRemoveModal = () => setShowRemoveModal(true);
   const handleCloseRemoveModal = () => setShowRemoveModal(false);
+  const handleShowRemoveContainersModal = () => setShowRemoveContainersModal(true);
+  const handleCloseRemoveContainersModal = () => setShowRemoveContainersModal(false);
+
+  const handleAssignContainer = () => {
+    handleCloseAssignModal();
+    // Add logic for assigning containers
+  };
+
+  const handleRemoveContainer = () => {
+    handleCloseRemoveModal();
+    // Add logic for removing container
+  };
+
+  const handleSeeContainers = (customerId) => {
+    console.log(`See Containers for customer ${customerId}`);
+    // Implement logic for "See Containers" action
+  };
+
+  const handleRemoveContainers = (customerId) => {
+    console.log(`Remove Containers for customer ${customerId}`);
+    setShowRemoveContainersModal(true);
+  };
+
+  const filteredCustomers = customerData.filter((customer) =>
+    customer.phoneNumber.includes(searchQuery)
+  );
 
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>Vendor Portal</h1>
-      <p>Containers Given Out: {containersGivenOut}</p>
-      <p>Assigned Containers: {assignedContainers}</p>
-      <button style={{ marginRight: '10px' }} onClick={handleShowAssignModal}>
-        Assign Container
-      </button>
-      <button onClick={handleShowRemoveModal}>
-        Remove Container
-      </button>
 
-      {/* add container */}
+      <Button variant="primary" style={{ margin: '10px' }} onClick={handleShowAssignModal}>
+        Assign Container
+      </Button>
+      <Button variant="danger" style={{ margin: '10px' }} onClick={handleShowRemoveModal}>
+        Remove Container
+      </Button>
+
+      {/* Assign Container Modal */}
       <Modal show={showAssignModal} onHide={handleCloseAssignModal}>
         <Modal.Header closeButton>
           <Modal.Title>Assign Container</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-
-
-
+          <Form>
+            <Form.Group controlId="customerId">
+              <Form.Label>Customer Phone Number:</Form.Label>
+              <Form.Control type="text" placeholder="Enter customer phone number" />
+            </Form.Group>
+            <Form.Group controlId="numContainersAssign">
+              <Form.Label>Number of Containers to Assign:</Form.Label>
+              <Form.Control type="number" min="1" placeholder="Enter number of containers" />
+            </Form.Group>
+            <Form.Group controlId="containerType">
+              <Form.Label>Container Type:</Form.Label>
+              <Form.Control as="select">
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseAssignModal}>
             Close
           </Button>
-         
+          <Button variant="primary" onClick={handleAssignContainer}>
+            Assign
+          </Button>
         </Modal.Footer>
       </Modal>
 
-      {/* remove container */}
-
+      {/* Remove Container Modal */}
       <Modal show={showRemoveModal} onHide={handleCloseRemoveModal}>
         <Modal.Header closeButton>
           <Modal.Title>Remove Container</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-
-
-          
+          <Form>
+            <Form.Group controlId="customerIdRemove">
+              <Form.Label>Customer Phone Number:</Form.Label>
+              <Form.Control type="text" placeholder="Enter customer phone number" />
+            </Form.Group>
+            <Form.Group controlId="numContainersRemove">
+              <Form.Label>Number of Containers to Remove:</Form.Label>
+              <Form.Control type="number" min="1" placeholder="Enter number of containers" />
+            </Form.Group>
+            <Form.Group controlId="containerTypeRemove">
+              <Form.Label>Container Type:</Form.Label>
+              <Form.Control as="select">
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseRemoveModal}>
             Close
           </Button>
-          
+          <Button variant="danger" onClick={handleRemoveContainer}>
+            Remove
+          </Button>
         </Modal.Footer>
       </Modal>
+
+      {/* Remove Containers Modal */}
+      <Modal show={showRemoveContainersModal} onHide={handleCloseRemoveContainersModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Remove Container</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="customerIdRemove">
+              <Form.Label>Customer Phone Number:</Form.Label>
+              <Form.Control type="text" placeholder="Enter customer phone number" />
+            </Form.Group>
+            <Form.Group controlId="numContainersRemove">
+              <Form.Label>Number of Containers to Remove:</Form.Label>
+              <Form.Control type="number" min="1" placeholder="Enter number of containers" />
+            </Form.Group>
+            <Form.Group controlId="containerTypeRemove">
+              <Form.Label>Container Type:</Form.Label>
+              <Form.Control as="select">
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseRemoveContainersModal}>
+            Close
+          </Button>
+          <Button variant="danger" onClick={handleRemoveContainer}>
+            Remove
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <p>Containers In Stock:</p>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Container Type</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(containersGivenOut).map(([type, quantity]) => (
+            <tr key={type}>
+              <td>{type}</td>
+              <td>{quantity}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+
+      {/* search bar */}
+      <input
+        type="text"
+        placeholder="Search by Phone Number"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <p>Containers Assigned:</p>
+      {/* Assigned Containers Table */}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Customer Name</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+            <th>Total Containers</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredCustomers.map((customer) => (
+            <tr key={customer.id}>
+              <td>{customer.name}</td>
+              <td>{customer.phoneNumber}</td>
+              <td>{customer.email}</td>
+              <td>{customer.totalContainers}</td>
+              <td>
+                <Button variant="primary" onClick={() => handleSeeContainers(customer.id)}>
+                  See Containers
+                </Button>{' '}
+                <Button variant="danger" onClick={() => handleRemoveContainers(customer.id)}>
+                  Remove Containers
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
