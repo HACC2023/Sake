8b96a487522a34e667507793631283502f8e8219
lamquangtruongnguyen <tnguyen10@my.hpu.edu>
2023-11-12 00:50:39 -1000

    modify toast msg user signin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,7 @@ const authUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(401);
-    throw new Error("Invalid email or password");
+    throw new Error("Invalid phone or password");
   }
 });
 
