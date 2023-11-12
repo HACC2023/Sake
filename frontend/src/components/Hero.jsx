b9b5de867da9c8066fd3c14650592b8950e24533
ksantiago3 <ksantiago3@my.hpu.edu>
2023-11-11 23:31:37 -1000

    Updated home page, About page, and contact us page.

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,47 +1,78 @@
-import { Carousel, Image } from "react-bootstrap";
-import "./Hero.css";
+import React from "react";
+import { Carousel, Image, Container } from "react-bootstrap";
+import Footer from "../components/Footer";
+
+const textShadowStyle = {
+  textShadow: '-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000',
+  color: 'white',
+};
 
 const Hero = () => {
+  document.body.style.backgroundColor = "#212529";
   return (
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        <Image src="https://www.honolulumagazine.com/content/uploads/2021/08/m/h/full-cycle-takeout-photo-by-erik-kabik.jpg" />
-        <Carousel.Caption>
-          <h3>
-            <strong>Oahu Zero Waste</strong>
-          </h3>
-          <p>Hawai’i’s first reusable takeout container program.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <Image src="https://images.squarespace-cdn.com/content/v1/607912e0b05e0b4b67ab64d8/4ff8fbd2-f26d-4bfd-83b3-7d009d55b699/Copy+of+IMG_8365.JPG" />
-        <Carousel.Caption>
-          <h3>
-            <strong>Our environment is our responsibility.</strong>
-          </h3>
-          <p>
-            Single-use takeout containers and food wrappers account for up to
-            one-third of the debris found on Hawai’i’s shorelines!! Offering a
-            reusable takeout container system helps to address this issue (and
-            many others) while also cultivating circular and zero waste
-            economies.
+    <div>
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <Image src="https://www.honolulumagazine.com/content/uploads/2021/08/m/h/full-cycle-takeout-photo-by-erik-kabik.jpg" />
+          <Carousel.Caption>
+            <h3 style={{ ...textShadowStyle, fontSize: '100px' }}>
+              <strong>Oahu Zero Waste</strong>
+            </h3>
+            <p style={{ ...textShadowStyle, fontSize: '30px' }}>
+              Hawai’i’s first reusable takeout container program.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <Image src="https://images.squarespace-cdn.com/content/v1/607912e0b05e0b4b67ab64d8/4ff8fbd2-f26d-4bfd-83b3-7d009d55b699/Copy+of+IMG_8365.JPG" />
+          <Carousel.Caption>
+            <h3 style={{ ...textShadowStyle, fontSize: '50px' }}>
+              <strong>Our environment is our responsibility.</strong>
+            </h3>
+            <p style={{ ...textShadowStyle, fontSize: '20px' }}>
+              Single-use takeout containers and food wrappers account for up to
+              one-third of the debris found on Hawai’i’s shorelines!! Offering a
+              reusable takeout container system helps to address this issue (and
+              many others) while also cultivating circular and zero waste
+              economies.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image src="https://images.squarespace-cdn.com/content/v1/607912e0b05e0b4b67ab64d8/6dc3146b-c896-4165-96e4-0e31ad3df49e/Copy+of+_DSC3598.jpg" />
+          <Carousel.Caption>
+            <h3 style={{ ...textShadowStyle, fontSize: '50px' }}>
+              <strong>Reusable Containers You Can Rent</strong>
+            </h3>
+            <p style={{ ...textShadowStyle, fontSize: '20px' }}>
+              We offer 5 different reusable takeout container options, 2
+              different reusable plate options, and 3 different reusable cutlery
+              options for event organizers.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      {/* The Mission Statement */}
+      <div
+        style={{
+          backgroundColor: "#2F5C86",
+          padding: "20px",
+          textAlign: "center",
+          borderRadius: "100px",
+        }}
+      >
+        <Container>
+          <h2 style={{ ...textShadowStyle, color: '#8CB890' }}>The Mission</h2>
+          <p style={{ ...textShadowStyle, color: 'white', fontSize: '18px' }}>
+            Our MISSION is to provide zero waste education, resources, and programs to build an ethical & responsible consumer culture.
           </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image src="https://images.squarespace-cdn.com/content/v1/607912e0b05e0b4b67ab64d8/6dc3146b-c896-4165-96e4-0e31ad3df49e/Copy+of+_DSC3598.jpg" />
-        <Carousel.Caption>
-          <h3>
-            <strong>Reusable Containers You Can Rent</strong>
-          </h3>
-          <p>
-            We offer 5 different reusable take out container options, 2
-            different reusable plate options, and 3 different reusable cutlery
-            options for event organizers.
+          <p style={{ ...textShadowStyle, color: 'white', fontSize: '18px' }}>
+            Our VISION is an island community where waste no longer exists.
           </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+        </Container>
+      </div>
+    </div>
   );
 };
 
--- frontend/src/screens/Contact.jsx
+++ frontend/src/screens/Contact.jsx
@@ -1,42 +1,67 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import Footer from "../components/Footer";
 
 const Contact = () => {
+  const contactContainerStyle = {
+    position: "absolute",
+    top: "20%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "rgba(255, 255, 255, 0.85)", // Adjust the background color and opacity as needed
+    padding: "20px",
+    borderRadius: "50px", // Set borderRadius to 0 to remove rounded corners
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)",
+    width: "60%", // Adjust the width as needed
+    maxWidth: "100%", // Ensure the container doesn't exceed the image width
+  };
+
+  const rowStyle = {
+    marginBottom: "20px", // Adjust the margin bottom as needed
+  };
+
   return (
-    <div>
-      {/* change this to edit the banner, the current one is kinda stretched */}
+    <div style={{ position: "relative" }}>
       <img
         src="https://images.squarespace-cdn.com/content/v1/5bb2972af4755a2a5eca6b7c/1538445058110-AJF7QG8H62P9UUZBIM89/OahuBeach_Above-2.jpg?format=2500w"
         alt="Banner"
-        style={{ width: "100%", height: 400 }} //feel free to change these values to adjust resolution
+        style={{ width: "100%", height: "auto" }}
       />
 
-      {/*contact details, the row column system is kinda confusing but once you got the hang of it you can figure it out*/}
-      <Container style={{ marginTop: "20px" }}>
-        <Row>
-          <Col>
-            <h2>Contact Us</h2>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <h3>Address</h3>
-            <p>10 Honolulu St</p>
-            <p>Oahu, 96826</p>
-          </Col>
-          <Col md={6}>
-            <h3>Phone</h3>
-            <p>(123) 456-7890</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h3>Email</h3>
-            <p> oahu.zerowaste@gmail.com</p>
-            <p> info@fullcycletakeout.org</p>
-          </Col>
-        </Row>
-      </Container>
+      {/* Contact details with grey background */}
+      <div style={contactContainerStyle}>
+        <Container>
+          <Row style={rowStyle}>
+            <Col>
+              <h2>Contact Us</h2>
+            </Col>
+          </Row>
+          <Row style={rowStyle}>
+            <Col md={6}>
+              <h3>Address</h3>
+              <p>10 Honolulu St</p>
+              <p>Oahu, 96826</p>
+            </Col>
+            <Col>
+              <h3>Email</h3>
+              <p>oahu.zerowaste@gmail.com</p>
+              <p>info@fullcycletakeout.org</p>
+            </Col>
+          </Row>
+          <Row style={rowStyle}>
+            <Col>
+              <h3>Form</h3>
+              <p>Alternatively, you use the following link below and fill out a form to address any inquiries.</p>
+              <p>
+                <a href="https://www.fullcycletakeouthawaii.org/get-in-touch" target="_blank">
+                https://www.fullcycletakeouthawaii.org/get-in-touch
+                </a>
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Footer />
     </div>
   );
 };
