8f189ace4a027b7c47c9118a3a9e9ca955ed3c2e
lamquangtruongnguyen <tnguyen10@my.hpu.edu>
2023-11-13 06:58:32 -1000

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -20,7 +21,15 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes, vendorRoutes, adminRoutes);
 
-app.get("/", (req, res) => res.send("Server is ready"));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Server is ready"));
+}
 
 // return a json object with error message and stack if in dev environment
 // instead of an HTML error page
