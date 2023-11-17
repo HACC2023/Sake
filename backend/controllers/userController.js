ebfa83a6802aed682aa858544b4bbbbbea44d070
lamquangtruongnguyen <tnguyen10@my.hpu.edu>
2023-11-16 15:28:56 -1000

    fix polulate not working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -82,8 +82,9 @@ const logoutUser = asyncHandler(async (req, res) => {
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findOne({ _id: req.user._id })
     .select("-password")
-    .populate({ path: "container.containerInfo" })
-    .populate({ path: "containerVendor" });
+    .populate("container.containerInfo")
+    .populate("containerVendor")
+    .exec();
   if (user) {
     res.status(200).json(user);
   } else {
